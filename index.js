117fb9dcd1d0ea7970cefe5430b1b43376913b13
Pablo-jesuss <pablo.programador26@gmail.com>
2023-10-09 22:07:44 -0300

    js

---
+++ index.js
@@ -0,0 +1,16 @@
+// Seleção de elementos 
+
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+
+
+
+
+
+
+
+
+// Funções
+
+// Eventos
