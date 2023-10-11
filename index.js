4894a3a408661c4bc6dd840f3b29f3aa8345e22d
Pablo-jesuss <pablo.programador26@gmail.com>
2023-10-10 22:52:34 -0300

    template

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ const createTable = (number, multiplicatorNumber) => {
   for(i = 1; i <= multiplicatorNumber; i++) {
     const result = number * i;
 
-    console.log(result);
+    const template = `<div class="row">
+    <div class="operation">${number} x ${i} = </div>
+    <div class="result">${result}</div>
+    </div>`;
   }
 
 }
