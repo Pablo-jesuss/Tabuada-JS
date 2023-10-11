c5ab17546fba4c81d5e2d64cca259a6447de560d
Pablo-jesuss <pablo.programador26@gmail.com>
2023-10-10 23:00:56 -0300

    innerText

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
 
+const multiplicationTitle = document.querySelector(
+  "#multiplication-title span"
+);
+
 const multiplicationTable = document.querySelector(
   "#multiplication-operations"
 );
@@ -29,8 +33,9 @@ const createTable = (number, multiplicatorNumber) => {
 
     multiplicationTable.appendChild(row);
 
-
   }
+
+  multiplicationTitle.innerText = number;
 
 }
 
