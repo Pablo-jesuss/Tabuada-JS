783cf170db62b00982d285a00f3755a689b4b81a
Pablo-jesuss <pablo.programador26@gmail.com>
2023-10-10 21:15:56 -0300

    js

--- index.js
+++ index.js
@@ -16,5 +16,14 @@ const multiplicationInput = document.querySelector("#multiplicator");
 // Eventos
 
 multiplicationForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const multiplicationNumber = +numberInput.value;
+
+  const multiplicatorNumber = +multiplicationInput.value;
+
+  if (!multiplicationNumber || !multiplicatorNumber) return;
+
+  console.log(multiplicationNumber, multiplicatorNumber);
   
 })
